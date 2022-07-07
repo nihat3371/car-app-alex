d291e5c3c3022ab6409561fef1f6320061cf1284
Nihat Aydın <08200000296@ogrenci.ege.edu.tr>
2022-07-07 14:26:57 +0300

    hey

--- src/App.js
+++ src/App.js
@@ -1,59 +1,9 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import { Link } from "react-router-dom";
-
-
+import React from 'react'
 
 const App = () => {
-
-
-  const [cars, setCars] = useState([])
-  const getCarData = async () => {
-    try {
-      const data = await axios.get("https://react-challenge-api.azurewebsites.net/vehicles")
-      setCars(data.data)
-    }
-    catch (e) {
-      console.log(e)
-    }
-  }
-
-  useEffect(() => {
-    getCarData()
-  }, [])
   return (
+    <div>Home</div>
+  )
+}
 
-    <div className="App">
-
-      <List sx={{ width: '100%', maxWidth: 600, bgcolor: 'background.paper' }}>
-        {cars.map((car) => (
-
-          <ListItemButton key={car.vin}>
-            <ListItem
-              key={car.vin}
-              disableGutters
-
-              secondaryAction={
-                <ListItemButton >
-                  <Link to={`/cardetails/${car.vin}`}>details</Link>
-
-                </ListItemButton>
-              }
-            >
-              <ListItemText key={car.vin} primary={car.model_variant} />
-
-            </ListItem>
-          </ListItemButton>
-
-        ))
-        }
-      </List >
-    </div >
-  );
-};
-
-export default App;
+export default App
--- src/pages/Cars.js
+++ src/pages/Cars.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import { Link } from "react-router-dom";
 
-const CarDetails = () => {
+
+
+const App = () => {
+
+
+    const [cars, setCars] = useState([])
+    const getCarData = async () => {
+        try {
+            const data = await axios.get("https://react-challenge-api.azurewebsites.net/vehicles")
+            setCars(data.data)
+        }
+        catch (e) {
+            console.log(e)
+        }
+    }
+
+    useEffect(() => {
+        getCarData()
+    }, [])
     return (
-        <div>CarDetails</div>
-    )
-}
 
+        <div className="App">
 
+            <List sx={{ width: '100%', maxWidth: 600, bgcolor: 'background.paper' }}>
+                {cars.map((car) => (
 
-export default CarDetails;
+                    <ListItemButton key={car.vin}>
+                        <ListItem
+                            key={car.vin}
+                            disableGutters
+
+                            secondaryAction={
+                                <ListItemButton >
+                                    <Link to={`/cardetails/${car.vin}`}>details</Link>
+
+                                </ListItemButton>
+                            }
+                        >
+                            <ListItemText key={car.vin} primary={car.model_variant} />
+
+                        </ListItem>
+                    </ListItemButton>
+
+                ))
+                }
+            </List >
+        </div >
+    );
+};
+
+export default App;
