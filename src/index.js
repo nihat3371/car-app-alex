0f6f28646e99ff9abc68aa8633fe34cfbc54eb0e
Nihat Aydın <08200000296@ogrenci.ege.edu.tr>
2022-07-12 19:29:55 +0300

    routers fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet, } from "react-router-dom";
 import DummyComponent from "./components/DummyComponent";
 import CarDetails from "./pages/CarDetails";
 import { Home } from "@mui/icons-material";
@@ -16,10 +16,9 @@ root.render(
 
   <BrowserRouter>
     <Routes>
-
-      <Route exact path="/" element={<App />} />
-      <Route path="CarDetails" element={<CarDetails />} />
-      <Route path="/cars" element={<Cars />}>
+      <Route path="/" element={<App />} />
+      <Route path="/cars" element={<Outlet />} >
+        <Route path="list" element={<Cars />} />
         <Route path=":vin" element={<CarDetails />} />
       </Route>
       <Route path="Home" element={<Home />} />
--- src/pages/CarDetails.js
+++ src/pages/CarDetails.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useParams } from "react-router-dom";
 
 const CarDetails = () => {
+
+
+    const { vin } = useParams();
+
     return (
-        <div>CarDetails</div>
+        <>
+            <h1>car</h1>
+            <ul>
+                this is your {vin}
+            </ul>
+        </>
     )
 }
 
