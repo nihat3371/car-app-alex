fa743ac059e82d8bf88b0990d3251d7f5b033b9e
Nihat Aydın <08200000296@ogrenci.ege.edu.tr>
2022-07-14 16:49:57 +0300

    packace update

--- src/pages/CarDetails.js
+++ src/pages/CarDetails.js
@@ -1,21 +1,38 @@
 import React from 'react'
-import { useParams } from "react-router-dom";
+import { useOutletContext, useParams } from "react-router-dom";
+import logo from '../images/coolcar.jpg'
 
 const CarDetails = () => {
-
-
     const { vin } = useParams();
+    const { cars } = useOutletContext();
+
+    const car = cars.find((car) => car.vin === vin);
+
+    if (!car) {
+        return "No car matches this VIN";
+    }
 
     return (
+
+
+
         <>
-            <h1>car</h1>
+            <div>
+                <h1>NO MATTER WHAT U CHOOSE THIS GUY HAS THE COOLEST CAR</h1>
+                <img src={logo} className="logo" alt='' />
+            </div>
+            <h1>{car.model_variant}</h1>
             <ul>
-                this is your {vin}
+                <li>Body: {car.body_type}</li>
+                <li>Doors: {car.doors}</li>
+                <li>Fuel: {car.fuel_type}</li>
+                <li>VIN: {car.vin}</li>
+                <li>Registration #: {car.regno}</li>
+                <li>Transmission: {car.transmission_type}</li>
             </ul>
         </>
-    )
-}
-
+    );
+};
 
 
 export default CarDetails;
